9759ee0c6d31891a17fa09f6ab75de97f27bb560
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-01-30 18:20:40 +0900

    不要なコメントの削除 & 重複箇所を関数化 & day => day_data へ変更 & rubocopの指摘を反映

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,24 +1,30 @@
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
-params = ARGV.getopts("y:","m:")
+params = ARGV.getopts('y:', 'm:')
 
-year = (params["y"] || Date.today.year).to_i # str型だとDate.new出来ないので
-month = (params["m"] || Date.today.month).to_i
+year = (params['y'] || Date.today.year).to_i
+month = (params['m'] || Date.today.month).to_i
 
-start_day = Date.new(year,month,1) # カレンダーで最初に表示する日
-end_day = Date.new(year,month,-1) # カレンダーで最後に表示する日
+start_day = Date.new(year, month, 1)
+end_day = Date.new(year, month, -1)
 
-# カレンダーのスタートは日曜日から始まり、最初に改行なしの空白を差し込めば、その曜日から出力を行える
-calendar_of_first_weeks_spaces = "   " * start_day.wday
+calendar_of_first_weeks_spaces = '   ' * start_day.wday
+
+def format_date(day_data)
+  "#{day_data.day.to_s.rjust(2)} "
+end
 
 puts "#{month}月 #{year}".center(20)
-puts " 日 月 火 水 木 金 土"
+puts '日 月 火 水 木 金 土'
 print calendar_of_first_weeks_spaces
-# 月の最初の日から、最後の日までループを回す
-(start_day..end_day).each{ |day|
-  # 土曜日でなければ、改行無しで出力
-  print day.day.to_s.rjust(3) unless day.wday == 6
-  # 土曜日は改行を行って出力
-  puts day.day.to_s.rjust(3) if day.wday == 6
-}
+
+(start_day..end_day).each do |day_data|
+  if day_data.wday == 6
+    puts format_date(day_data)
+  else
+    print format_date(day_data)
+  end
+end
