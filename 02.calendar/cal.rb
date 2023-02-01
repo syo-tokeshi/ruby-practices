0cf027c52de95902a213aa1c4a6470a6c1086147
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-01 15:31:00 +0900

    変数名の修正 + 土曜日かどうかの判定を変更 + 土曜日の後のスペースを削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -11,17 +11,15 @@ month = (params['m'] || Date.today.month).to_i
 start_date = Date.new(year, month, 1)
 end_date = Date.new(year, month, -1)
 
-calendar_of_first_weeks_spaces = '   ' * start_date.wday
+first_week_spaces = '   ' * start_date.wday
 
 puts "#{month}月 #{year}".center(20)
 puts '日 月 火 水 木 金 土'
-print calendar_of_first_weeks_spaces
+print first_week_spaces
 
 (start_date..end_date).each do |date|
   formatted_day = "#{date.day.to_s.rjust(2)} "
-  if date.wday == 6
-    puts formatted_day
-  else
-    print formatted_day
-  end
+  formatted_day.slice!(-1) if date.saturday?
+  print formatted_day
+  print "\n" if date.saturday?
 end
