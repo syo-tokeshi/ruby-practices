f8a1eff6e0956cba5ef7e6cf64029f36b9db70b5
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-01 11:37:35 +0900

    クラス名に合わせたインスタンス変数名に変更 + 関数をローカル変数に変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,23 +8,20 @@ params = ARGV.getopts('y:', 'm:')
 year = (params['y'] || Date.today.year).to_i
 month = (params['m'] || Date.today.month).to_i
 
-start_day = Date.new(year, month, 1)
-end_day = Date.new(year, month, -1)
+start_date = Date.new(year, month, 1)
+end_date = Date.new(year, month, -1)
 
-calendar_of_first_weeks_spaces = '   ' * start_day.wday
-
-def format_date(day_data)
-  "#{day_data.day.to_s.rjust(2)} "
-end
+calendar_of_first_weeks_spaces = '   ' * start_date.wday
 
 puts "#{month}月 #{year}".center(20)
 puts '日 月 火 水 木 金 土'
 print calendar_of_first_weeks_spaces
 
-(start_day..end_day).each do |day_data|
-  if day_data.wday == 6
-    puts format_date(day_data)
+(start_date..end_date).each do |date|
+  formatted_day = "#{date.day.to_s.rjust(2)} "
+  if date.wday == 6
+    puts formatted_day
   else
-    print format_date(day_data)
+    print formatted_day
   end
 end
