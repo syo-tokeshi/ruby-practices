97e9d36fb1f840cf7e543560d31c882874356aa5
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-09 15:42:40 +0900

    不要なコメントの削除 + 変数の定数化 + timesメソッドをeachに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,21 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'debug'
-
 files_in_current_path = Dir.glob('*')
-
-# カレントディレクトリにあるファイルを全て受け取って、表示する列数で割って、1列辺りの数と余りを取得
+COLUMN_COUNT_FILE_SHOW = 3
 def file_display_to_required_info(files_in_current_path)
-  column_count_file_show = 3
-  files_in_current_path.count.divmod(column_count_file_show)
+  files_in_current_path.count.divmod(COLUMN_COUNT_FILE_SHOW)
 end
 
 number_column_show, remainder_number = file_display_to_required_info(files_in_current_path)
 
 number_column_show += 1 if remainder_number.positive?
 
-# 全部のファイル群を、1列辺りの平均値で、配列を作成
 files_to_display = files_in_current_path.each_slice(number_column_show).to_a
 
 file_and_file_between_space = 7
@@ -24,12 +19,10 @@ def row_filename_max_length(row)
   row.map(&:size).max
 end
 
-# 表示する列の最大幅を決める
 displayed_file_max_name_numbers = files_to_display.map do |row|
   row_filename_max_length(row) + file_and_file_between_space
 end
 
-# グループ化したファイル群を、ljustで、左揃えさせる(その際に、そのグループのファイル名の最大文字数を考慮する)
 adjusted_displayed_files = files_to_display.map.with_index do |row, index|
   row.map do |file_name|
     file_name.ljust(displayed_file_max_name_numbers[index])
@@ -38,9 +31,9 @@ end
 
 adjusted_displayed_files_by_index_number = adjusted_displayed_files[0].zip(*adjusted_displayed_files[1..])
 
-adjusted_displayed_files_by_index_number.each do |column|
-  column.count.times do |index|
-    print column[index]
+adjusted_displayed_files_by_index_number.each do |columns|
+  columns.each do |column|
+    print column
   end
   print "\n"
 end
