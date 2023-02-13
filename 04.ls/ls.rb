670c3675a5fb037dae560aef0ffff9208a5ba9c4
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-13 19:41:37 +0900

    シンボリックの表示 + バイトサイズの整列まで対応

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,6 +3,7 @@
 
 require 'optparse'
 require "etc"
+require "debug"
 
 TABLE_NUMERIC_ALPHABET = {
   '0' => '-',
@@ -17,6 +18,7 @@ TABLE_NUMERIC_ALPHABET = {
 TABLE_FILETYPE_ALPHABET = {
   '100' => '-',
   '40' => 'd',
+  '120' => 'l'
 }.freeze
 
 def make_authority_alphabetic(permissions)
@@ -29,7 +31,7 @@ def change_number_to_file_type(file_type)
   TABLE_FILETYPE_ALPHABET[file_type]
 end
 
-def get_array_of_file_type_and_permissions(string_of_file_type_and_permissions)
+def get_file_type_and_permissions(string_of_file_type_and_permissions)
   user = string_of_file_type_and_permissions.slice!(-1)
   group = string_of_file_type_and_permissions.slice!(-1)
   owner = string_of_file_type_and_permissions.slice!(-1)
@@ -73,10 +75,50 @@ end
 
 adjusted_displayed_files_by_index_number = adjusted_displayed_files[0].zip(*adjusted_displayed_files[1..])
 
+def display_file_type_and_permission(file)
+  file_hold_detail = File::Stat.new(File.open(file))
+  string_of_file_type_and_permissions =  file_hold_detail.mode.to_s(8)
+  array_of_file_type_and_permissions = get_file_type_and_permissions(string_of_file_type_and_permissions)
+  file_type = change_number_to_file_type(array_of_file_type_and_permissions.last)
+  permissions = make_authority_alphabetic(array_of_file_type_and_permissions[0..2])
+  if FileTest.symlink?(file)
+    file_type = "l"
+  end
+  [file_type,permissions].flatten.join
+end
+
+def get_max_byte_count_display_path(files_in_current_path)
+  files_max_byte_number = files_in_current_path.map do |file|
+    File::Stat.new(File.open(file)).size.to_s
+  end
+  files_max_byte_number.map(&:size).max
+end
+
+def show_remaining_detail(file,max_byte_count_display_path)
+  file_hold_detail = File::Stat.new(File.open(file))
+  hard_link_count = file_hold_detail.nlink
+  owner_name = Etc.getpwuid(file_hold_detail.uid).name
+  group_name = Etc.getgrgid(file_hold_detail.gid).name
+  bite_size = file_hold_detail.size.to_s.rjust(max_byte_count_display_path)
+  updated_date = file_hold_detail.mtime.strftime('%_m月 %_d %H:%M')
+  [hard_link_count,owner_name,group_name,bite_size,updated_date].flatten.join("  ")
+end
+
+max_byte_count_display_path = get_max_byte_count_display_path(files_in_current_path)
+
 if params['l']
-  adjusted_displayed_files_by_index_number.each do |columns|
+  adjusted_displayed_files.each do |columns|
     columns.each do |column|
-      print column
+      if column
+        removed_space_column = column.strip
+        # debugger
+        if FileTest.symlink?(removed_space_column)
+          symbolic_name_to_display = "#{removed_space_column} -> #{File.readlink(removed_space_column)}"
+          puts "#{display_file_type_and_permission(removed_space_column)} #{show_remaining_detail(removed_space_column,max_byte_count_display_path)} #{symbolic_name_to_display}"
+        else
+          puts "#{display_file_type_and_permission(removed_space_column)} #{show_remaining_detail(removed_space_column,max_byte_count_display_path)} #{removed_space_column}"
+        end
+      end
     end
   end
 else
