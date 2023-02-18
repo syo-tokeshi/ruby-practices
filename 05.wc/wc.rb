946e54a9d5c7114ac4d9c9c1359e84d11548b889
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-18 19:32:31 +0900

    [完]渡されたファイルをwcで3オプション分実行する処理

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -12,10 +12,41 @@ def main
   end
 end
 
+# 引数で渡されたファイルを調べる
 def file_count
-  p ARGV
+  loaded_files = ARGV.map do
+    File.read(_1)
+  end
+  count_lines(loaded_files)
+  count_words(loaded_files)
+  count_characters(loaded_files)
 end
 
+# 行数
+def count_lines(files)
+  files_lines = files.map do
+    _1.lines.count
+  end
+  p files_lines
+end
+
+# 単語数
+def count_words(files)
+  files_words = files.map do
+    _1.split(/\s+/).size
+  end
+  p files_words
+end
+
+# 文字数
+def count_characters(files)
+  files_characters = files.map do
+    _1.split(/\s+/).join.chars.size
+  end
+  p files_characters
+end
+
+# 標準入力を調べる
 def input_count(params)
   p params
 end
