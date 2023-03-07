1d5785465ff09a24241985c2bd50f7f7a2369eac
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-07 19:09:13 +0900

    rubocopの指摘を反映

--- 06.bowling_object/score.rb
+++ 06.bowling_object/score.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 class Score
   attr_reader :scores
+
   def initialize(command_line_args)
-    raise ArgumentError.new("1-9の範囲の数字、またはXを渡して下さい")  if unexpected_value?(command_line_args)
+    raise ArgumentError, '1-9の範囲の数字、またはXを渡して下さい' if unexpected_value?(command_line_args)
+
     divided_scores = divide_scores(command_line_args)
     processed_scores = process_score(divided_scores)
     @scores = processed_scores.flatten
   end
 
   def unexpected_value?(command_line_args)
-    command_line_args.split(",").grep_v(/[0-9X]/).any?
+    command_line_args.split(',').grep_v(/[0-9X]/).any?
   end
 
   def divide_scores(command_line_args)
@@ -20,5 +24,4 @@ class Score
       score == 'X' ? %w[10 0] : score
     end
   end
-
 end
