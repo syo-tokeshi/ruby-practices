80d69d5dd6a1de6e2932d9c54732dc4ba3fda601
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-17 11:22:57 +0900

    責務を考え、一部メソッドを抽出する

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -17,13 +17,12 @@ class Ls
 
   def output
     files = get_files_from_path(@path, @is_dotmatch)
-    files.reverse! if @is_reversed
     @is_detailed ? output_detail(files) : output_without_detail(files)
   end
 
   def get_files_from_path(path, is_dotmatch)
     dotmatch_pattern = is_dotmatch ? File::FNM_DOTMATCH : 0
-    if path.nil?
+    files = if path.nil?
       Dir.glob('*', dotmatch_pattern)
     elsif FileTest.directory? path
       Dir.glob(File.join(path, '*'), dotmatch_pattern)
@@ -32,6 +31,7 @@ class Ls
     else
       raise ArgumentError "ls: #{ARGV[0]}: No such file or directory"
     end
+    @is_reversed ? files.reverse! : files
   end
 
   def output_detail(files)
@@ -52,8 +52,8 @@ class Ls
   end
 
   def align_files(file_informations, added_space = 1, right_justified_flag: true)
-    word_counts = file_informations.map { |file_info| (file_info.bytesize + file_info.length) / 2 }
-    max_length = word_counts.max + added_space
+    word_counts = get_word_counts(file_informations)
+    max_length = get_max_length(added_space, word_counts)
     file_informations.map do |file_info|
       if right_justified_flag
         file_info.rjust(max_length)
@@ -64,9 +64,9 @@ class Ls
   end
 
   def transpose_file_names(file_names, column_count = 3)
-    row_count = file_names.length.quo(column_count).ceil
-    sliced_file_names = file_names.each_slice(row_count).to_a
-    (row_count - sliced_file_names[-1].length).times { sliced_file_names[-1] << '' }
+    row_count = get_row_count(column_count, file_names)
+    sliced_file_names = slice_file_names(file_names, row_count)
+    offset_for_transpose(row_count, sliced_file_names)
     sliced_file_names.transpose
   end
 
@@ -132,6 +132,26 @@ class Ls
   end
 
   private
+
+  def offset_for_transpose(row_count, sliced_file_names)
+    (row_count - sliced_file_names[-1].length).times { sliced_file_names[-1] << '' }
+  end
+
+  def slice_file_names(file_names, row_count)
+    file_names.each_slice(row_count).to_a
+  end
+
+  def get_row_count(column_count, file_names)
+    file_names.length.quo(column_count).ceil
+  end
+
+  def get_max_length(added_space, word_counts)
+    word_counts.max + added_space
+  end
+
+  def get_word_counts(file_informations)
+    file_informations.map { |file_info| (file_info.bytesize + file_info.length) / 2 }
+  end
 
   def get_file_names(files)
     files.map { |file| File.basename(file) }
